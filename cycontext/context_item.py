d26b38f8c440e45faf34630544ec90b6c3bc7329
Alec Chapman <abchapman93@gmail.com>
2019-11-28 11:53:01 -0700

    add test module for context_item

--- cycontext/context_item.py
+++ cycontext/context_item.py
@@ -1,10 +1,10 @@
-class ItemData:
-    """An ItemData defines a ConText modifier. It defines the phrase to be matched,
+class ConTextItem:
+    """An ConTextItem defines a ConText modifier. It defines the phrase to be matched,
     the category/semantic class, and the rule which the modifier executes.
     """
     _ALLOWED_RULES = ("forward", "backward", "bidirectional", "terminate")
     def __init__(self, literal, category, pattern=None, rule="bidirectional", comment=''):
-        """Create an ItemData object.
+        """Create an ConTextItem object.
 
         literal (str): The actual string of a concept. If pattern is None,
             this string will be matched exactly.
@@ -13,7 +13,7 @@ class ItemData:
             See https://spacy.io/usage/rule-based-matching.
         rule (str): The directionality or action of a modifier.
             One of ("forward", "backward", "bidirectional", or "terminate").
-        RETURNS (ItemData)
+        RETURNS (ConTextItem)
         """
         self.literal = literal
         self.category = category.lower()
@@ -25,7 +25,7 @@ class ItemData:
             raise ValueError("Rule {0} not recognized. Must be one of: {1}".format(self.rule, self._ALLOWED_RULES))
 
     def __repr__(self):
-        return f"ItemData: [{self.literal}, {self.category}, {self.pattern}, {self.rule}]"
+        return f"ConTextItem: [{self.literal}, {self.category}, {self.pattern}, {self.rule}]"
 
 
 def from_json(filepath):
@@ -33,7 +33,7 @@ def from_json(filepath):
 
     filepath (text): the .json file containing modifier rules
 
-    RETURNS item_data (list): a list of ItemData objects
+    RETURNS item_data (list): a list of ConTextItem objects
     """
     import json
     with open(filepath) as f:
@@ -41,6 +41,6 @@ def from_json(filepath):
 
     item_data = []
     for data in modifier_data["patterns"]:
-        item = ItemData(**data) # TODO: this will throw an error if there are any erroneous keys
+        item = ConTextItem(**data) # TODO: this will throw an error if there are any erroneous keys
         item_data.append(item)
     return item_data
---
+++ tests/test_context_item.py
@@ -0,0 +1,80 @@
+import pytest
+
+from cycontext import ConTextItem
+from cycontext.context_item import from_json, from_dict
+
+class TestItemData:
+
+    def test_instantiate1(self):
+        literal = "no evidence of"
+        category = "DEFINITE_NEGATED_EXISTENCE"
+        rule = "forward"
+        assert ConTextItem(literal, category, rule)
+
+    def test_context_item_category_upper(self):
+        """Test that a ConTextItem category is always upper"""
+        literal = "no evidence of"
+        category = "definite_negated_existence"
+        rule = "forward"
+        item = ConTextItem(literal, category, rule)
+        assert item.category == "DEFINITE_NEGATED_EXISTENCE"
+
+    def test_context_item_rule_upper(self):
+        """Test that a ConTextItem rule is always upper"""
+        literal = "no evidence of"
+        category = "definite_negated_existence"
+        rule = "forward"
+        item = ConTextItem(literal, category, rule)
+        assert item.rule == "FORWARD"
+
+    def test_rule_value_error(self):
+        """Test that ConTextItem raises a ValueError if an invalid rule is passed in."""
+        literal = "no evidence of"
+        category = "definite_negated_existence"
+        rule = "asdf"
+        with pytest.raises(ValueError):
+            ConTextItem(literal, category, rule)
+
+
+    def test_from_dict(self):
+        d = dict(literal="reason for examination", category="INDICATION", rule="FORWARD")
+        assert from_dict(d)
+
+    def test_from_dict_error(self):
+        d = dict(literal="reason for examination", category="INDICATION", rule="FORWARD",
+                 invalid="this is an invalid key")
+        with pytest.raises(ValueError):
+            from_dict(d)
+
+    def test_from_json(self):
+        modifiers_filepath = self.raw_and_clean_data_file("./tmp")
+        item_data = from_json(modifiers_filepath)
+
+    @pytest.fixture
+    def raw_and_clean_data_file(self, tmpdir):
+        import json, os
+        json_filepath = tmpdir.join("modifiers.json")
+
+        patterns = [
+            {
+                "literal": "are ruled out",
+                "category": "DEFINITE_NEGATED_EXISTENCE",
+                "pattern": None,
+                "rule": "backward"
+            },
+            {
+                "literal": "is negative",
+                "category": "DEFINITE_NEGATED_EXISTENCE",
+                "pattern": [
+                    {"LEMMA": "be"}, {"LOWER": "negative"}
+                ],
+                "rule": "backward"
+            },
+        ]
+
+        # Save dicts to a temporary file
+        with open(json_filepath, "w") as f:
+            json.dump({"patterns": patterns}, f)
+
+        yield json_filepath
+        os.remove(json_filepath)
