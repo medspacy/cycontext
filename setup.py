7ff482f81430d4b9c5d74e31851883807780f4e2
Alec Chapman <abchapman93@gmail.com>
2020-10-19 16:09:04 -0600

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cycontext",
-    version="1.0.3.1",
+    version="1.0.3.2",
     description="ConText algorithm using spaCy for clinical NLP",
     author="medSpaCy",
     author_email="medspacy.dev@gmail.com",
